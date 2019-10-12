361ad2f9534e3f313304dfe07c4733b78d8a0aee
Juan Fonseca <juanma2268@gmail.com>
2019-10-12 17:58:44 -0600

    Added proper GPL 3.0 license

--- src/batchProcessing/contourBatch.py
+++ src/batchProcessing/contourBatch.py
@@ -1,9 +1,23 @@
+"""
+Copyright (C) 2019 Juan M. Fonseca-Solis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Correspondence concerning ARAPSUAA should be addressed as follows:
+    Website: https://github.com/juanfonsecasolis/ARAPSUAA
+    Linkedin: https://cr.linkedin.com/in/juan-m-fonseca-solis
+"""
+
 # -*- coding: utf-8 -*-
-# Author: Juan Manuel Fonseca-Solís 2018 (juan[dot]fonsecasolis[at]ucr[dot]ac[dot]cr)
-# Project: ARAPSUAA: Automatic recognition of accessible pedestrian signals using an adaptive approach
-# Website: https://github.com/juanfonsecasolis/ARAPSUAA
-# Get your free copy of the thesis at http://hdl.handle.net/2238/10642
-#
 from __future__ import print_function
 from __future__ import print_function
 from __future__ import print_function
